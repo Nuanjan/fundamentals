c73d8e99bf0b14a5c010f1e43571964b7f063bdb
Nuanjan <nuanjan.schluntz@gmail.com>
2021-09-26 20:13:40 -0400

    uncomment code

--- for-loops-basic-1/loop_demo.py
+++ for-loops-basic-1/loop_demo.py
@@ -1,36 +1,36 @@
 # Basic - Print all integers from 0 to 150.
-# for x in range(0, 151):
-#     print(x)
+for x in range(0, 151):
+    print(x)
 
 # Multiples of Five - Print all the multiples of 5 from 5 to 1,000
-# for x in range(5,1001):
-#     if x % 5 == 0:
-#         print(x)
+for x in range(5,1001):
+    if x % 5 == 0:
+        print(x)
 
 # Counting, the Dojo Way - Print integers 1 to 100. 
 # If divisible by 5, print "Coding" instead. 
 # If divisible by 10, print "Coding Dojo".
 
-# for x in range(1, 101):
-#     if x % 10 == 0:
-#         print("Coding Dojo")
-#     elif x % 5 == 0:
-#         print("Coding")
-#     else:
-#         print(x)
+for x in range(1, 101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else:
+        print(x)
 
 
 # Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 
-# num = 0
-# for x in range(0, 500000):
-#     num = num + x
-# print(num)
+num = 0
+for x in range(0, 500000):
+    num = num + x
+print(num)
 
-# Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
 
-# for x in range(2018, 0, -4):
-#     print(x)
+for x in range(2018, 0, -4):
+    print(x)
 
 
 # Flexible Counter - Set three variables: lowNum, 
