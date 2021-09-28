b9014aea8c5385e679d3c2d0b2b3945d93d41162
Nuanjan <nuanjan.schluntz@gmail.com>
2021-09-28 01:28:20 -0400

    make a method chain by return self

--- oop/user/class_user_demo.py
+++ oop/user/class_user_demo.py
@@ -5,28 +5,24 @@ class User:		# here's what we have so far
         self.account_balance = 0
     # adding the deposit method
     def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
-    	self.account_balance += amount	# the specific user's account increases by the amount of the value received
+        self.account_balance += amount	# the specific user's account increases by the amount of the value received
+        return self
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: {self.account_balance}")
+        return self
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
     
 
 user1 = User("Noon", "noon@moon.com")
-user1.make_deposit(100)
-user1.make_deposit(500)
-user1.make_withdrawal(50)
 user2 = User("Bee", "bee@hive.com")
-user2.make_deposit(2000)
+user1.make_deposit(100).make_deposit(500).make_withdrawal(50).display_user_balance().transfer_money(user2, 50)
+user2.make_deposit(2000).display_user_balance()
 
-user1.display_user_balance()
-user2.display_user_balance()
-
-user1.transfer_money(user2, 50)
-user1.display_user_balance()
-user2.display_user_balance()
 
