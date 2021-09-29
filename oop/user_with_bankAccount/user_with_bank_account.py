c235c05a19fc7e2b240fc867393c66f80b5c46ef
Nuanjan <nuanjan.schluntz@gmail.com>
2021-09-29 16:41:01 -0400

    add multiply account to user class

--- oop/user_with_bankAccount/user_with_bank_account.py
+++ oop/user_with_bankAccount/user_with_bank_account.py
@@ -1,8 +1,11 @@
 class BankAccount:
     bank_name = "Nation Bank"
+    #class attribute, everytime bank account got created it will put into it
+    accounts= []
     def __init__(self, int_rate=0.03, balance=0):
         self.int_rate = int_rate
         self.balance = balance
+        BankAccount.accounts.append(self)
     def deposit(self, amount):
         self.balance += amount
         return self
@@ -24,20 +27,33 @@ class BankAccount:
         print(f"Bank Name: {cls.blank_name}")
 
 class User:		# here's what we have so far
-    def __init__(self, name, email):
+    def __init__(self, name, email,accountType):
         self.name = name
         self.email = email
+        self.accountType = accountType
         self.account = BankAccount(int_rate=0.05, balance=0)
+        self.savingAccount = BankAccount(int_rate=0.03, balance=0)
     # adding the deposit method
     def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
-        self.account.deposit(amount)	# the specific user's account increases by the amount of the value received
+        if self.accountType == "saving":
+            self.savingAccount.deposit(amount)	# the specific user's account increases by the amount of the value received
+        else:
+            self.account.deposit(amount)
         return self
     def make_withdrawal(self, amount):
-        self.account.withdraw(amount)
+        if self.accountType == "saving":
+            self.savingAccount.withdraw(amount)
+        else:
+            self.account.withdraw(amount)
         return self
     def display_user_balance(self):
         print(f"User: {self.name}", end=' ')
-        self.account.display_account_info()
+        if self.accountType == "saving":
+            print(f"Saving Account:", end=' ')
+            self.savingAccount.display_account_info()
+        else:
+            print(f"Account:", end=' ')
+            self.account.display_account_info()
         return self
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
@@ -45,7 +61,7 @@ class User:		# here's what we have so far
         return self
 
 
-user1 = User("Noon", "noon@moon.com")
+user1 = User("Noon", "noon@moon.com","saving")
 user1.make_deposit(100).make_deposit(100).display_user_balance()
-user2 = User("Roy", "roy@boy.com")
+user2 = User("Roy", "roy@boy.com","account")
 user2.make_deposit(100).make_withdrawal(50).make_deposit(500).display_user_balance()
