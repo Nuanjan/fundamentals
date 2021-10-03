597a95aa6e1e42c6375de054b60f1576138252ae
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-03 18:55:51 -0400

    add more instant objects.

--- oop/math_dojo/math_dojo.py
+++ oop/math_dojo/math_dojo.py
@@ -27,3 +27,11 @@ md = MathDojo()
 x = md.add(2).add(2, 5, 1).subtract(3, 2).result
 print(x)  # should print 5
 # run each of the methods a few more times and check the result!
+
+md1 = MathDojo()
+y = md1.add(4, 2).add(1).add(5, 7, 8).result  # expected 27
+print(y)
+
+md2 = MathDojo()
+z = md2.subtract(1).subtract(2, 3).subtract(5, 6, -2).result  # expected -15
+print(z)
